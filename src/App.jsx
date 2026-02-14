48e7c119791a38a94c4b041f5bedf043702ca86c
sarahroon <sarahrooney130500@gmail.com>
2026-02-14 18:38:18 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import ThumbnailGallery from "./components/ThumbnailGallery";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <h1>Frog Image Gallery</h1>
       <ThumbnailGallery />
-
-      <div className="counter">
-        <button onClick={() => setCount(count + 1)}>count is {count}</button>
-      </div>
     </div>
   );
 }
