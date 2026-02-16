27fd857c9ff5d77f2658a01b76c7cf485e2fbfd2
SARAH ROONEY <SARAHROONEY130500@GMAIL.COM>
2026-02-16 13:49:44 +0000

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import "./buttons.jsx";
 import "./App.css";
+import "./App.jsx";
+import "./main.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
